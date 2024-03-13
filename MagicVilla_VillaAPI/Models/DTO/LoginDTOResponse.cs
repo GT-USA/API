4c49e4f5a44a1e5eacebff3e288443d5758e2552
GirayTurker <tug88418@temple.edu>
2024-03-13 09:40:22 -0400

    Models for Login and Registration

---
+++ MagicVilla_VillaAPI/Models/DTO/LoginDTOResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_VillaAPI.Models.DTO
+{
+    public class LoginDTOResponse
+    {
+        public LocalUser User { get; set; }
+        public string Token { get; set; }
+    }
+}
