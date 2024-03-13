ed3b716a392aa8d9276c8d56464a7b88a44132ac
GirayTurker <tug88418@temple.edu>
2024-03-13 17:30:43 -0400

    User Repository

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_VillaAPI.Models.DTO;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool isUniqueUser(string username);
+        
+        //Login return type
+        Task<LoginDTOResponse> Login(LoginDTORequest loginDTORequest);
+        
+        //Return user that has been created in DB
+        Task<LocalUser>Register(RegisterationDTORequest registerationDTORequest);
+    }
+}
---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models.DTO;
+using MagicVilla_VillaAPI.Repository.IRepository;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository :IUserRepository
+    {
+        private readonly AppDBContext _db;
+        public UserRepository(AppDBContext db)
+        {
+            _db = db;
+        }
+
+        public bool isUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginDTOResponse> Login(LoginDTORequest loginDTORequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationDTORequest registerationDTORequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
